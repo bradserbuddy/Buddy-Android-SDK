15cbcac98f43985d21dcd50affd1b2ecff01cd2b
Nick Ambrose <nick@buddy.com>
2014-08-26 16:58:58 -0700

    Added Location Model

---
+++ library/src/main/java/com/buddy/sdk/models/BuddyLocation.java
@@ -0,0 +1,18 @@
+package com.buddy.sdk.models;
+
+/**
+ * Created by nick on 8/26/14.
+ */
+public class BuddyLocation {
+    public String name;
+    public String description;
+    public String address1;
+    public String address2;
+    public String city;
+    public String region;
+    public String country;
+    public String fax;
+    public String phone;
+    public String website;
+    public double distanceFromSearch;
+}
